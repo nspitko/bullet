b1c71e3d7c19402ee598454d6496f1bc3141a02b
Spitko <fireslash@fireslash.net>
2022-08-16 23:24:20 -0700

    Fix compile errors, add missing functions

--- bullet/Generator.hx
+++ bullet/Generator.hx
@@ -8,6 +8,8 @@ class Generator {
 #pragma warning(disable:4305)
 #pragma warning(disable:4244)
 #pragma warning(disable:4316)
+#undef min
+#undef max
 #endif
 #include <btBulletDynamicsCommon.h>
 #include <BulletSoftBody/btSoftBody.h>
